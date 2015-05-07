1781a2e6ea26aee102a690e4cb724e3695c488c6
seppeydavid <seppeydavid@gmail.com>
2015-05-07 19:32:33 +0200

    Initial Commit

---
+++ Smart_Rider_2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Smart_Rider_2
+//
+//  Created by Macmini_Dagon on 07.05.15.
+//  Copyright (c) 2015 Macmini_Dagon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Smart_Rider_2Tests/Smart_Rider_2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Smart_Rider_2Tests.swift
+//  Smart_Rider_2Tests
+//
+//  Created by Macmini_Dagon on 07.05.15.
+//  Copyright (c) 2015 Macmini_Dagon. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Smart_Rider_2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
